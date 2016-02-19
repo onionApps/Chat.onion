1bccd2816e09a1d8fc51fb7231192ce8ed8d95e6
user <email@example.com>
2016-02-19 11:46:46 +0100

    -

--- app/src/main/java/onion/chat/Utils.java
+++ app/src/main/java/onion/chat/Utils.java
@@ -1,5 +1,9 @@
 /*
  * Chat.onion - P2P Instant Messenger
+ *
+ * http://play.google.com/store/apps/details?id=onion.chat
+ * http://onionapps.github.io/Chat.onion/
+ *
  * Author: http://github.com/onionApps - http://jkrnk73uid7p5thz.onion - bitcoin:1kGXfWx8PHZEVriCNkbP5hzD15HS4AyKf
  */
 
