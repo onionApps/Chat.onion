dbf6435e1d893622e039b4e99eba2b1c84adfd3c
user <email@example.com>
2016-02-19 11:56:51 +0100

    -

--- app/src/main/java/onion/chat/Notifier.java
+++ app/src/main/java/onion/chat/Notifier.java
@@ -3,6 +3,7 @@
  *
  * http://play.google.com/store/apps/details?id=onion.chat
  * http://onionapps.github.io/Chat.onion/
+ * http://github.com/onionApps/Chat.onion
  *
  * Author: http://github.com/onionApps - http://jkrnk73uid7p5thz.onion - bitcoin:1kGXfWx8PHZEVriCNkbP5hzD15HS4AyKf
  */
